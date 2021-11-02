161669fee54c6c873e2074186cccabb49838c2c3
Mathieu <48250246+MatjoVS@users.noreply.github.com>
2021-11-02 16:07:51 -0400

    mat

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
-function calculateSubtotal(product){
-  let price = product.querySelector('.price span').innerHTML
-  let quantity = product.querySelector('.quantity input').value
-  console.log(price)
-  console.log(quantity)
-  let subtotal = price*quantity
-  console.log(subtotal)
-  return subtotal
+function calculateSubtotal(product) {
+  let price = product.querySelector('.price span').innerHTML;
+  let quantity = product.querySelector('.quantity input').value;
+  console.log(price);
+  console.log(quantity);
+  let subtotal = price * quantity;
+  console.log(subtotal);
+  return subtotal;
 }
 
 document.querySelector('.calculate-total').onclick = function (e) {
-  console.log()
-calculateSubtotal(document)
+  console.log();
+  calculateSubtotal(document);
+};
+function calculateAll() {
+  let allProducts = document.getElementsByClassName('product');
+  for (let i = 0; i < allProducts.length; i++) {
+    calculateSubtotal(allProducts[i]);
+  }
 }
